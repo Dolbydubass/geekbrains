9b491419445d6793b80ab3a66fbba8f43fcb3dc7
dolbydubass <fvvperm@gmail.com>
2019-09-26 12:27:57 +0500

    lesson 5 easy

---
+++ lesson_5_easy.py
@@ -0,0 +1,28 @@
+# Задача-1:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+# из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
+import os
+
+
+def new_folders():
+    return [os.mkdir('dir_' + str(i)) for i in range(1, 10)]
+
+
+def del_new_folders():
+    return [os.removedirs('dir_' + str(i)) for i in range(1, 10)]
+
+
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+
+def show_folders():
+    return [i for i in os.listdir() if os.path.isdir(i)]
+
+
+# Задача-3:
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copy_file():
+    path = os.getcwd()
+    return os.system('cp test.py test_copy.py')
