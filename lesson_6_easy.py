d698bfc57774fb7ed23546f7c9851c964968e355
dolbydubass <fvvperm@gmail.com>
2019-10-01 13:49:33 +0500

    fix bool

--- lesson_6_easy.py
+++ lesson_6_easy.py
@@ -1,9 +1,9 @@
 class Car:
-    def __init__(self, car_name, car_color, car_speed, is_police):
+    def __init__(self, car_name, car_color, car_speed, is_police: bool):
         self.name = car_name
         self.color = car_color
         self.speed = car_speed
-        self.is_police = is_police
+        self.is_police = bool(is_police)
 
     def go(self):
         print('Машина поехала')
@@ -16,16 +16,48 @@ class Car:
 
 
 class TownCar(Car):
+    def __init__(self, car_name, car_color, car_speed, is_police: bool):
+        super().__init__(car_name, car_color, car_speed, is_police=False)
+
+
+class SportCar(TownCar):
     pass
 
 
-class SportCar(Car):
-    pass
-
-
-class WorkCar(Car):
+class WorkCar(TownCar):
     pass
 
 
 class PoliceCar(Car):
-    pass
+    def __init__(self, car_name, car_color, car_speed, is_police: bool):
+        super().__init__(car_name, car_color, car_speed, is_police=True)
+
+
+# Тест:
+prius = TownCar('Prius', 'Белый', '60 км/ч', '')
+prius.go()
+print(prius.name)
+print(prius.color)
+print(prius.speed)
+print(prius.is_police)
+print()
+mercedes = SportCar('Mercedes', 'Черный', '210 км/ч', '')
+mercedes.stop()
+print(mercedes.name)
+print(mercedes.color)
+print(mercedes.speed)
+print(mercedes.is_police)
+print()
+solaris = TownCar('Solaris', 'Синий', '60 км/ч', '')
+solaris.go()
+print(solaris.name)
+print(solaris.color)
+print(solaris.speed)
+print(solaris.is_police)
+print()
+ford = PoliceCar('Ford', 'LAPD', '110 км/ч', '')
+ford.turn('налево')
+print(ford.name)
+print(ford.color)
+print(ford.speed)
+print(ford.is_police)
